14bb44a6be64f889b1c0e23ee7575df11609285d
szwldev <14846869@qq.com>
2021-03-16 18:21:27 +0800

    devicebean

---
+++ src/main/java/com/adamin/appserver/netty/bean/DeviceSessionBean.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C), 2020-2021
+ * FileName: DeviceSessionBean
+ * Date:     2021/3/16 18:17
+ * Description: 客户端
+ */
+package com.adamin.appserver.netty.bean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.channels.Channel;
+
+/**
+ *
+ * @author adamin
+ * @site https://www.lixiaopeng.top
+ * @create 2021/3/16 18:17
+ */
+public class DeviceSessionBean {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DeviceSessionBean.class);
+    private String sn;
+    private String appName; //hook的应用名称
+    private Channel channel=null;
+    private long lastHeartBeatTimeStamp;
+
+    public static Logger getLOGGER() {
+        return LOGGER;
+    }
+
+    public String getSn() {
+        return sn;
+    }
+
+    public void setSn(String sn) {
+        this.sn = sn;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public Channel getChannel() {
+        return channel;
+    }
+
+    public void setChannel(Channel channel) {
+        this.channel = channel;
+    }
+
+    public long getLastHeartBeatTimeStamp() {
+        return lastHeartBeatTimeStamp;
+    }
+
+    public void setLastHeartBeatTimeStamp(long lastHeartBeatTimeStamp) {
+        this.lastHeartBeatTimeStamp = lastHeartBeatTimeStamp;
+    }
+}
